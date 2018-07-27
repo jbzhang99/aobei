13dcab60b0430a3103a3268f7871d0a23aa64caf
buliwen <buliwen@aobei.com>
2018-07-27 14:22:57 +0800

    test rejectunit分支

--- train-console/src/main/java/com/aobei/BootApplication.java
+++ train-console/src/main/java/com/aobei/BootApplication.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @EnableScheduling
 @EnableCaching
 public class BootApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BootApplication.class, args);
 	}
