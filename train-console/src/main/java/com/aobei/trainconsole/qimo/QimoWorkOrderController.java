5b4e11bd8086c38b799343fb0bd89617d1997723
buliwen <buliwen@aobei.com>
2018-08-06 12:15:18 +0800

    change：七陌流出接口修改

--- train-console/src/main/java/com/aobei/trainconsole/qimo/QimoWorkOrderController.java
+++ train-console/src/main/java/com/aobei/trainconsole/qimo/QimoWorkOrderController.java
@@ -21,22 +21,18 @@ public class QimoWorkOrderController {
     private static final Logger logger = LoggerFactory.getLogger(QimoWorkOrderController.class);
 
     @PostMapping("/receiveWorkOrder")
-    public String receiveWorkOrder(@RequestHeader("Authorization") String authorization,
-                                   InputStream inputStream,
-                                   @RequestParam String sig) {
+    public String receiveWorkOrder(InputStream inputStream) {
         String returnCode = "400";
         try {
-            logger.info("authorization:{} sig:{}", authorization, sig);
             // 获取JSON 数据
             String workOrdersJson = StreamUtils.copyToString(inputStream, Charset.forName("utf-8"));
             logger.info("Qimo receiveWorkOrder DATA:{}", workOrdersJson);
             // 请求签名校验
-            if (QimoServer.sigValidate(authorization, sig)) {
-                JSONObject jsonObject = JSONObject.parseObject(workOrdersJson);
-                QimoReceiveRequestBody receivedWorkOrder = jsonObject.toJavaObject(QimoReceiveRequestBody.class);
+            JSONObject jsonObject = JSONObject.parseObject(workOrdersJson);
+            QimoReceiveRequestBody receivedWorkOrder = jsonObject.toJavaObject(QimoReceiveRequestBody.class);
+            //存储工单数据
 
-                returnCode = "200";
-            }
+            returnCode = "200";
         } catch (Exception e) {
             logger.error("Qimo receiveWorkOrder ERROR", e);
         }
