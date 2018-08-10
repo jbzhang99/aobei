dd7b95f78898e22a1ec1393edca612ac573ea0b4
liqizhen <liqizhen@aobei.com>
2018-08-10 19:30:17 +0800

    添加redis,存储顾客领取优惠券信息

--- trainapi/src/main/java/com/aobei/trainapi/server/impl/ApiOrderServiceImpl.java
+++ trainapi/src/main/java/com/aobei/trainapi/server/impl/ApiOrderServiceImpl.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alipay.api.AlipayApiException;
 import com.aobei.common.bean.SmsData;
 import com.aobei.common.boot.EventPublisher;
+import com.aobei.common.boot.RedisIdGenerator;
 import com.aobei.common.boot.event.SmsSendEvent;
 import com.aobei.train.IdGenerator;
 import com.aobei.train.Roles;
@@ -26,6 +27,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import weixin.popular.bean.paymch.MchPayApp;
@@ -93,6 +95,10 @@ public class ApiOrderServiceImpl implements ApiOrderService {
     PushHandler pushHandler;
     @Autowired
     OnsHandler onsHandler;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+    @Autowired
+    private RedisIdGenerator redisIdGenerator;
     @Override
     public void paySuccess(Order order, int paytype) {
         // 只需要更新order表中的订单支付状态表
@@ -296,6 +302,13 @@ public class ApiOrderServiceImpl implements ApiOrderService {
                     .andDeletedEqualTo(0);
             List<CouponReceive> couponReceives = couponReceiveService.selectByExample(couponReceiveExample);
             logger.info("api-method:getCounpons:process couponReceives:{}", couponReceives);
+            RedisIdGenerator idGenerator = new RedisIdGenerator();
+            idGenerator.setRedisTemplate(redisTemplate);
+            long autoIncrId = idGenerator.getAutoIncrNum(coupon_id+customer.getCustomer_id()+"");
+            if (autoIncrId != 1){
+                response.setErrors(Errors._42029);
+                return response;
+            }
             //目前需求一个优惠券一个用户只可以领取一次
             if (couponReceives.size() == 0){
                 CouponReceive couponReceive = new CouponReceive();
